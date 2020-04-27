fca19e51229e3ca90636f0fe202ce9fa0fd69fc3
Slava <kreeon@ya.ru>
2020-04-27 22:09:54 +0300

    settings

--- gulp-tasks/smart-grid.js
+++ gulp-tasks/smart-grid.js
@@ -1,39 +1,35 @@
-"use strict";
+'use strict';
 
-import gulp from "gulp";
-const smartgrid = require("smart-grid");
+import gulp from 'gulp';
+const smartgrid = require('smart-grid');
 
-gulp.task("smart-grid", (cb) => {
-    smartgrid("./src/styles/vendor/import/", {
-        outputStyle: "scss",
-        filename: "_smart-grid",
-        columns: 12, // number of grid columns
-        offset: "30px", // gutter width - 30px
-        // mobileFirst: true,
-        // mixinNames: {
-        //     container: "container"
-        // },
-        container: {
-            maxWidth: "1620px",
-            fields: "30px", // side fields - 15px
-        },
-        breakPoints: {
-            xs: {
-                width: "480px", // 320px
-            },
-            sm: {
-                width: "640px", // 576px
-            },
-            md: {
-                width: "900px", // 768px
-            },
-            lg: {
-                width: "1200px", // 992px
-            },
-            xl: {
-                width: "1500px", // 1200px
-            },
-        },
-    });
-    cb();
+gulp.task('smart-grid', (cb) => {
+  smartgrid('./src/styles/vendor/import/', {
+    outputStyle: 'scss',
+    filename: '_smart-grid',
+    columns: 12, // number of grid columns
+    offset: '30px', // gutter width - 30px
+    container: {
+      maxWidth: '1620px',
+      fields: '30px', // side fields - 15px
+    },
+    breakPoints: {
+      xs: {
+        width: '480px', // 320px
+      },
+      sm: {
+        width: '640px', // 576px
+      },
+      md: {
+        width: '900px', // 768px
+      },
+      lg: {
+        width: '1200px', // 992px
+      },
+      xl: {
+        width: '1500px', // 1200px
+      },
+    },
+  });
+  cb();
 });
